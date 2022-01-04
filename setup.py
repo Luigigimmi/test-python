0a3fae14b9b92c43c8c3269d600670c2cb3c9fc9
Luigigimmi <gimmiluigi@gmail.com>
2022-01-04 21:26:36 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test-python',
+    version='1.0.0',
+    url='https://github.com/Luigigimmi/test-python',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    packages=find_packages(),
+    python_requires=">=3.8"
+)
